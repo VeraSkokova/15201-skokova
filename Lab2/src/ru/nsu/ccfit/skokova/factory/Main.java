ef751304d1296d0a73cd0e319743934f47af4454
veraskokova <vera-skokova11@yandex.ru>
2017-06-14 07:43:25 +0600

    Fixed Main

--- Lab2/src/ru/nsu/ccfit/skokova/factory/Main.java
+++ Lab2/src/ru/nsu/ccfit/skokova/factory/Main.java
@@ -1,44 +1,35 @@
 package ru.nsu.ccfit.skokova.factory;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import ru.nsu.ccfit.skokova.factory.gui.CreateAndShowGUI;
-import ru.nsu.ccfit.skokova.factory.gui.FactoryButtons;
-import ru.nsu.ccfit.skokova.factory.gui.FactoryInformer;
-import ru.nsu.ccfit.skokova.factory.gui.FactoryRegulator;
 import ru.nsu.ccfit.skokova.threadpool.ThreadPool;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
-
-    private static Logger logger = LogManager.getLogger(Main.class);
-
     static {
         System.getProperties().setProperty("log4j.configurationFile", "src/log4j2.xml");
     }
 
+    private static Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         try {
             if (args.length != 1) {
-                System.out.println("Add configuration file");
+              System.out.println("Add configuration file");
             } else {
-                String name = args[0];
-                ConfigParser configParser = new ConfigParser(name);
+              String name = args[0];
+              ConfigParser configParser = new ConfigParser(name);
 
-                FactoryController factoryController = new FactoryController(configParser);
+              FactoryController factoryController = new FactoryController(configParser);
+              Controller controller = new Controller(factoryController);
+          }
 
-                Controller controller = new Controller(factoryController);
 
-
-            } catch(IOException | BadParseException | NumberFormatException e){
-                logger.error(e.getMessage());
-            }
+        } catch (IOException | BadParseException | NumberFormatException e) {
+            logger.error(e.getMessage());
         }
+
     }
 }
